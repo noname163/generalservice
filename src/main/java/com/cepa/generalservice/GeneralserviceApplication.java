859a854ea3a842b364c7bbec75680f8c4d66ecf5
DangHuuDat <danghuudat163@gmail.com>
2023-08-28 09:13:10 +0700

    create(CEPA-50): create base project

---
+++ src/main/java/com/cepa/generalservice/GeneralserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cepa.generalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cepa/generalservice/GeneralserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cepa.generalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
