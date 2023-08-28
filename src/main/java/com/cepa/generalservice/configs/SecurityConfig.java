b534c114f5da4eee562864295d1370e07dc75749
DangHuuDat <danghuudat163@gmail.com>
2023-08-28 16:50:22 +0700

    feat(CEPA-51): Add swagger ui - Create SecurityConfig, OpenAPIConfig

---
+++ src/main/java/com/cepa/generalservice/configs/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.cepa.generalservice.configs;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.context.WebApplicationContext;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public SecurityContext securityContext() {
+        return SecurityContextHolder.getContext();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // @Bean
+    // public SecurityFilterChain filterChain(
+    //         HttpSecurity httpSecurity, AuthenticationFilter authenticationFilter,
+    //         ExceptionHandlerFilter exceptionHandlerFilter) throws Exception {
+    //     httpSecurity.csrf().disable().cors();
+    //     httpSecurity.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    //     httpSecurity.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    //     httpSecurity.addFilterBefore(exceptionHandlerFilter, AuthenticationFilter.class);
+    //     return httpSecurity.build();
+    // }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().antMatchers("/swagger-ui/**", "/v3/api-docs/**");
+    }
+}
