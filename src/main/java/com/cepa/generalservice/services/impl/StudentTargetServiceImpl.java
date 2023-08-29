0ad712ea6319dcfa8ba22b351853e04d1c187487
DangHuuDat <danghuudat163@gmail.com>
2023-08-29 10:35:23 +0700

    feat(CEPA-46): Create register feature - Add TargetRepository, StudentTargetService, AuthenticationController - Modify RegisterService, SecurityConfig, UserInformationRepository, StudentTargetRepository

---
+++ src/main/java/com/cepa/generalservice/services/impl/StudentTargetServiceImpl.java
@@ -0,0 +1,50 @@
+package com.cepa.generalservice.services.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cepa.generalservice.data.entities.StudentTarget;
+import com.cepa.generalservice.data.entities.Subject;
+import com.cepa.generalservice.data.entities.UserInformation;
+import com.cepa.generalservice.data.repositories.StudentTargetRepository;
+import com.cepa.generalservice.data.repositories.SubjectRepository;
+import com.cepa.generalservice.exceptions.BadRequestException;
+import com.cepa.generalservice.services.StudentTargetService;
+
+@Service
+public class StudentTargetServiceImpl implements StudentTargetService {
+    @Autowired
+    private StudentTargetRepository studentTargetRepository;
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    @Override
+    @Transactional
+    public void createStudentTarget(UserInformation userInformation, List<Long> subjectIds) {
+        List<Subject> subjects = subjectRepository.findByIdIn(subjectIds);
+        List<StudentTarget> existingStudentTargets = studentTargetRepository.findByStudentInformation(userInformation);
+
+        List<StudentTarget> newStudentTargets = new ArrayList<>();
+        for (Subject subject : subjects) {
+            boolean subjectAlreadyExists = existingStudentTargets.stream()
+                    .anyMatch(studentTarget -> studentTarget.getSubject().equals(subject));
+
+            if (subjectAlreadyExists) {
+                throw new BadRequestException("Subject " + subject.getName() + " already exists.");
+            }
+
+            newStudentTargets.add(StudentTarget.builder()
+                    .studentInformation(userInformation)
+                    .subject(subject)
+                    .build());
+        }
+
+        studentTargetRepository.saveAll(newStudentTargets);
+    }
+
+}
