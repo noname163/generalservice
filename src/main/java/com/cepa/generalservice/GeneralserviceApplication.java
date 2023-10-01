2e2d31f5730fab27767165337d2389fdccc57a1a
DangHuuDat <danghuudat163@gmail.com>
2023-10-01 10:24:58 +0700

    feat(CEPA-92): Add discovery service

--- src/main/java/com/cepa/generalservice/GeneralserviceApplication.java
+++ src/main/java/com/cepa/generalservice/GeneralserviceApplication.java
@@ -2,8 +2,10 @@ package com.cepa.generalservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GeneralserviceApplication {
 
 	public static void main(String[] args) {
